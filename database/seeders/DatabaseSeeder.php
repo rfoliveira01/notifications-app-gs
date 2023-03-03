336a9114580fdf55834782b3379496a584bf1930
rodrigofo <rodrigo.fo1990@gmail.com>
2023-03-03 17:34:13 -0300

    Readme

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             CategoriesSeeder::class,
-            SubscribersSeeder::class
+            UsersSeeder::class
         ]);
     }
 }
